108a03839d036d5cb250e8d6108d26fe4e1ea838
Markus Falk <mail@markus-falk.com>
2018-11-21 07:26:20 +0100

    :sparkles: add template files
    refs  #2

--- src/ng-dockerize/index.ts
+++ src/ng-dockerize/index.ts
@@ -1,10 +1,25 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, url, template, branchAndMerge, mergeWith } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+// import { Schema as ClassOptions } from './schema';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngDockerize(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+
+    console.log(tree);
+    
+    const templateSource = apply(
+      url('./files'),
+      [
+        template({
+          ...strings,
+          ..._options,
+        }),
+      ]
+    );
+ 
+    return branchAndMerge(mergeWith(templateSource));
   };
 }
